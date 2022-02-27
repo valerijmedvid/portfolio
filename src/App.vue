2a98e35d5d0f8d3b06e3adcfa3d32dce75d25d4d
Valerij Medviď <vmedvid@gmail.com>
2022-02-27 22:20:35 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="">
+    <a href=""> <div class="">Github</div> </a>
+    <a href=""> <div class="">LinkedIn</div> </a>
+    <a href=""> <div class="">Twitter</div> </a>
+  </div>
+</template>
+<script setup></script>
