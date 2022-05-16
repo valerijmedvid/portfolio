9c688ff4fa4be4a356343cb17299c92d08e9bb1e
Valerij Medviď <vmedvid@gmail.com>
2022-05-16 14:56:52 +0200

    add GoogleAnalytics

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # vili-portfolio
+![Deploy](https://github.com/valerijmedvid/portfolio/workflows/Deploy/badge.svg)
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from "vue"
 import App from "./App.vue"
+import VueGtag from "vue-gtag"
 
-createApp(App).mount("#app")
+createApp(App)
+  .use(VueGtag, {
+    config: { id: "G-DF9S1ND9KZ" },
+  })
+  .mount("#app")
