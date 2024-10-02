d9de5c3317555b6516be8ea7071f6c2576f0cdc7
valerijmedvid <vmedvid@gmail.com>
2024-10-02 20:33:21 +0200

    add experience

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -15,31 +15,38 @@ type ExperienceList = Work[]
 
 const Experiences: ExperienceList = [
   {
+    company: 'Komerční banka',
+    position: 'Frontend developer',
+    link: 'https://www.kb.cz/',
+    period: { start: new Date('2024-01-01'), end: null },
+    description: 'TS · Tribe · Git · JavaScript · Vue',
+  },
+  {
     company: 'Košík s.r.o.',
     position: 'Frontend developer',
     link: 'https://www.kosik.cz/',
-    period: { start: new Date('2022-06-1'), end: null },
-    description: 'Scrum · Komunikace · JS · SQL · Git · JavaScript · Vue · Docker',
+    period: { start: new Date('2022-06-01'), end: new Date('2023-12-31') },
+    description: 'Scrum · Komunikace · SQL · Git · JavaScript · Vue · Docker',
   },
   {
     company: 'Avast s.r.o.',
     position: 'Software Engineer',
     link: 'https://www.avast.com/',
-    period: { start: new Date('2019-03-1'), end: new Date('2022-05-31') },
+    period: { start: new Date('2019-03-01'), end: new Date('2022-05-31') },
     description: 'Python · JS · PostgreSQL · SQL · Git · JavaScript · Vue · Docker',
   },
   {
     company: 'Avast s.r.o.',
     position: 'Senior Threat Operations Analyst',
     link: 'https://www.avast.com/',
-    period: { start: new Date('2012-10-1'), end: new Date('2019-02-28') },
+    period: { start: new Date('2012-10-01'), end: new Date('2019-02-28') },
     description: 'Python · JS · SQL · Git · Laravel',
   },
   {
     company: 'APS Czech Republic a.s.',
     position: 'IT & Database specialist',
     link: 'https://cz.aps-holding.com/',
-    period: { start: new Date('2011-10-1'), end: new Date('2012-09-30') },
+    period: { start: new Date('2011-10-01'), end: new Date('2012-09-30') },
     description:
       'IT administration, configuration & installation. Database development & optimization.',
   },
@@ -50,7 +57,7 @@ export default function WorkExperience() {
     <div className='m-12 font-sans text-center'>
       <h2 className='font-sans text-2xl text-center font-bold mb-10'>Work experience</h2>
       {Experiences.map((experience, index) => (
-        <article key={index} className='mb-8'>
+        <article key={`${index}-${experience.company}`} className='mb-8'>
           <h3 className='font-bold accent'>
             {experience.position}
             <a href={experience.link}>
@@ -61,7 +68,7 @@ export default function WorkExperience() {
           </h3>
           <p className='text-gray-500'>
             {experience.period.start.getFullYear()} -{' '}
-            {experience.period.end ? experience.period.end.getFullYear() : 'presence'}
+            {experience.period.end ? experience.period.end.getFullYear() : 'now'}
           </p>
           <p>{experience.description}</p>
         </article>
